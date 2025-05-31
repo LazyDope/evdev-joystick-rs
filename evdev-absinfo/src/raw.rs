d8b8c00d2857d84ad975db2933a8a4b8426462a4
LazyDope <mydj555@gmail.com>
2025-05-31 00:53:58 -0400

    remove unnecessary imports

--- evdev-absinfo/src/raw.rs
+++ evdev-absinfo/src/raw.rs
@@ -1,30 +1,6 @@
 use libc::input_absinfo;
 use nix::{request_code_read, request_code_write};
 
-const IOC_NRBITS: u32 = 8;
-const IOC_TYPEBITS: u32 = 8;
-cfg_if::cfg_if! {
-    if #[cfg(any(
-        any(target_arch = "powerpc", target_arch = "powerpc64"),
-        any(target_arch = "sparc", target_arch = "sparc64"),
-        any(target_arch = "mips", target_arch = "mips64"),
-    ))] {
-        const IOC_SIZEBITS: u32 = 13;
-        const IOC_DIRBITS: u32 = 3;
-
-        pub const IOC_NONE: u32 = 0b1;
-        pub const IOC_READ: u32 = 0b10;
-        pub const IOC_WRITE: u32 = 0b100;
-    } else {
-        const IOC_SIZEBITS: u32 = 14;
-        const IOC_DIRBITS: u32 = 2;
-
-        pub const IOC_NONE: u32 = 0b0;
-        pub const IOC_WRITE: u32 = 0b1;
-        pub const IOC_READ: u32 = 0b10;
-    }
-}
-
 pub const unsafe fn evioc_set_abs(axis_index: u16) -> u64 {
     request_code_write!(
         b'E' as u32,
