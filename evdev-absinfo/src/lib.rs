db76a7d0f8faa01291a0b13f289889323ccd6c63
LazyDope <mydj555@gmail.com>
2025-05-31 02:14:49 -0400

    add checks for axis abs bits

--- evdev-absinfo/src/lib.rs
+++ evdev-absinfo/src/lib.rs
@@ -1,14 +1,20 @@
-use std::{fmt::Display, fs::File, mem::MaybeUninit, os::fd::AsRawFd};
+use std::{
+    fmt::Display,
+    fs::File,
+    mem::MaybeUninit,
+    os::fd::{AsRawFd, RawFd},
+};
 
 use libc::{ABS_MAX, input_absinfo as CAbsInfo};
 use nix::errno::Errno;
 
 mod raw;
+use raw::AbsBitMask;
 
 #[derive(Debug)]
-struct AbsInfo {
+pub struct AbsInfo {
     axis: u16,
-    inner: CAbsInfo,
+    pub inner: CAbsInfo,
 }
 
 impl AbsInfo {
@@ -20,11 +26,9 @@ impl AbsInfo {
     }
 
     fn get_absinfo(file: &File, axis_index: u16) -> Result<Self, Error> {
-        if axis_index > ABS_MAX {
-            return Err(Error::AxisErr(axis_index));
-        }
-        let mut abs_info: MaybeUninit<CAbsInfo> = MaybeUninit::uninit();
         let fd = file.as_raw_fd();
+        check_valid_axis(fd, axis_index)?;
+        let mut abs_info: MaybeUninit<CAbsInfo> = MaybeUninit::uninit();
         let int_result = unsafe { libc::ioctl(fd, raw::evioc_get_abs(axis_index), &mut abs_info) };
         Errno::result(int_result)?;
         Ok(AbsInfo {
@@ -34,12 +38,35 @@ impl AbsInfo {
     }
 }
 
+fn check_valid_axis(fd: RawFd, axis_index: u16) -> Result<(), Error> {
+    // Check axis is within range
+    if axis_index > ABS_MAX {
+        return Err(Error::UnboundAxis(axis_index));
+    }
+
+    // Check axis is an abs axis
+    let mut abs_bitmask: MaybeUninit<AbsBitMask> = MaybeUninit::uninit();
+    unsafe { raw::evioc_get_abs_bit(fd, abs_bitmask.as_mut_ptr()) }?;
+    let abs_bitmask = unsafe { abs_bitmask.assume_init() };
+    if !test_axis(axis_index, &abs_bitmask) {
+        return Err(Error::NonAbsAxis(axis_index));
+    }
+    Ok(())
+}
+
+#[inline(always)]
+const fn test_axis(axis_index: u16, bit_array: &AbsBitMask) -> bool {
+    bit_array[(axis_index / 8) as usize] & (1 << (axis_index % 8)) != 0
+}
+
 #[derive(thiserror::Error, Debug)]
 enum Error {
     #[error("ioctl error: {0}")]
-    CErr(#[from] Errno),
+    CErrno(#[from] Errno),
     #[error("invalid axis: {0}, must be between 0 and {ABS_MAX}")]
-    AxisErr(u16),
+    UnboundAxis(u16),
+    #[error("invalid axis: {0}, must be a valid ABS axis for this device")]
+    NonAbsAxis(u16),
 }
 
 impl Display for AbsInfo {
@@ -56,7 +83,7 @@ impl Display for AbsInfo {
                     ..
                 },
         } = self;
-        let flat_percent = flat as f64 / (maximum - minimum) as f64 * 100.;
+        let flat_percent = f64::from(flat) / f64::from(maximum - minimum) * 100.;
         write!(
             f,
             "Absolute axis {0:#x} ({0}) (value: {1}, min: {2}, max: {3}, flatness: {4} (={5:.2}%), fuzz: {6})",
@@ -65,21 +92,53 @@ impl Display for AbsInfo {
     }
 }
 
+// these tests only work on my machine until I can read all devices
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    #[ignore]
+    fn test_read_and_write() {
         let mut file = File::open("/dev/input/by-id/usb-Thrustmaster_TWCS_Throttle-event-joystick")
             .expect("This device is available");
         let mut abs_info = AbsInfo::get_absinfo(&file, 2).expect("Axis 2 on this device is valid");
         println!("{}", abs_info);
         abs_info.inner.maximum /= 2;
+        let temp = abs_info.inner.maximum;
         abs_info
             .set_absinfo(&mut file)
             .expect("Setting the maximum value to half should always succeed");
-        let mut abs_info = AbsInfo::get_absinfo(&file, 2).expect("Axis 2 on this device is valid");
+        let abs_info = AbsInfo::get_absinfo(&file, 2).expect("Axis 2 on this device is valid");
+        assert_eq!(abs_info.inner.maximum, temp);
         println!("{}", abs_info);
+    }
+
+    #[test]
+    fn test_read() {
+        let file = File::open("/dev/input/by-id/usb-Thrustmaster_TWCS_Throttle-event-joystick")
+            .expect("This device is available");
+        let abs_info = AbsInfo::get_absinfo(&file, 2).expect("Axis 2 on this device is valid");
+        println!("{}", abs_info);
+    }
+
+    #[test]
+    fn test_out_of_bounds_axis() {
+        let file = File::open("/dev/input/by-id/usb-Thrustmaster_TWCS_Throttle-event-joystick")
+            .expect("This device is available");
+        assert!(matches!(
+            AbsInfo::get_absinfo(&file, ABS_MAX + 1),
+            Err(Error::UnboundAxis(_))
+        ));
+    }
+
+    #[test]
+    fn test_invalid_axis() {
+        let file = File::open("/dev/input/by-id/usb-Thrustmaster_TWCS_Throttle-event-joystick")
+            .expect("This device is available");
+        assert!(matches!(
+            AbsInfo::get_absinfo(&file, 8),
+            Err(Error::NonAbsAxis(_))
+        ));
     }
 }
--- evdev-absinfo/src/raw.rs
+++ evdev-absinfo/src/raw.rs
@@ -1,10 +1,18 @@
-use libc::input_absinfo;
-use nix::{request_code_read, request_code_write};
+use libc::{ABS_MAX, input_absinfo};
+use nix::{ioctl_read, request_code_read, request_code_write};
 
-pub const unsafe fn evioc_set_abs(axis_index: u16) -> u64 {
-    request_code_write!(b'E' as u32, 0xc0 + axis_index, size_of::<input_absinfo>())
+const EV_IOC_MAGIC: u8 = b'E';
+// EVIOCSABS from linux/input.h
+pub const fn evioc_set_abs(axis_index: u16) -> u64 {
+    request_code_write!(EV_IOC_MAGIC, 0xc0 + axis_index, size_of::<input_absinfo>())
 }
 
-pub const unsafe fn evioc_get_abs(axis_index: u16) -> u64 {
-    request_code_read!(b'E' as u32, 0x40 + axis_index, size_of::<input_absinfo>())
+// EVIOCGABS from linux/input.h
+pub const fn evioc_get_abs(axis_index: u16) -> u64 {
+    request_code_read!(EV_IOC_MAGIC, 0x40 + axis_index, size_of::<input_absinfo>())
 }
+
+// EVIOCGBIT from linux/input.h using EV_ABS from linux/input-event-codes.h
+const EV_ABS: u8 = 0x03;
+pub type AbsBitMask = [u8; ABS_MAX as usize / 8 + 1];
+ioctl_read!(evioc_get_abs_bit, EV_IOC_MAGIC, 0x20 + EV_ABS, AbsBitMask);
