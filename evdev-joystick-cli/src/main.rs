ed52f945bafc336c79e5b62b4cefb8527db2c171
LazyDope <mydj555@gmail.com>
2025-07-11 22:30:52 -0400

    improve joystick button support

--- evdev-joystick-cli/src/main.rs
+++ evdev-joystick-cli/src/main.rs
@@ -13,18 +13,37 @@ struct Args {
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let joystick = Joystick::new_from_path(args.device)?;
-    for event in joystick
-        .events()
-        .filter(|event| event.is_type(&EventType::EV_ABS))
-    {
+    for event in joystick.events() {
         let InputEvent {
-            time, event_code, ..
+            time,
+            event_code,
+            value,
         } = event;
-        let abs_info = joystick.abs_info(&event_code).unwrap();
-        println!(
-            "{}.{}: code {}, {}",
-            time.tv_sec, time.tv_usec, event_code, abs_info
-        );
+        match event.event_type() {
+            Some(EventType::EV_ABS) => {
+                let abs_info = joystick
+                    .abs_info(&event_code)
+                    .expect("Joystick axis must be enabled");
+                println!(
+                    "{}.{}: code {}, {}",
+                    time.tv_sec, time.tv_usec, event_code, abs_info
+                );
+            }
+            Some(EventType::EV_KEY) => {
+                println!(
+                    "{}.{}: code BTN_{:?}, {}",
+                    time.tv_sec,
+                    time.tv_usec,
+                    joystick
+                        .get_button_index(&event_code)
+                        .expect("Button pressed must be enabled")
+                        + 1,
+                    value
+                );
+            }
+            Some(_) => (),
+            None => (),
+        }
     }
     Ok(())
 }
